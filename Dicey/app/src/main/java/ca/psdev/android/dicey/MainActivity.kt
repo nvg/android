b66177fc787dcc282bb59adb333c73860ba29286
NG <nick.goupinets@gmail.com>
2020-04-21 17:10:36 -0400

    Added orientation handling

--- Dicey/app/src/main/java/ca/psdev/android/dicey/MainActivity.kt
+++ Dicey/app/src/main/java/ca/psdev/android/dicey/MainActivity.kt
@@ -1,37 +1,57 @@
 package ca.psdev.android.dicey
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.concurrent.ThreadLocalRandom
-import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private var mDice1 = 1
+    private var mDice2 = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
 
-        btn_roll.setOnClickListener {
-            val dice1 = ThreadLocalRandom.current().nextInt(1, 6)
-            val dice2 = ThreadLocalRandom.current().nextInt(1, 6)
-
-            iv_diceLeft.setImageDrawable(
-                ContextCompat.getDrawable(
-                    this,
-                    getResources().getIdentifier("dice" + dice1, "drawable", getPackageName())
-                )
-            )
-
-            iv_diceRight.setImageDrawable(
-                ContextCompat.getDrawable(
-                    this,
-                    getResources().getIdentifier("dice" + dice2, "drawable", getPackageName())
-                )
-            )
+        if (savedInstanceState != null) {
+            mDice1 = savedInstanceState.getInt("dice1")
+            mDice2 = savedInstanceState.getInt("dice2")
+            redrawDice()
         }
+
+        btn_roll.setOnClickListener {
+            mDice1 = ThreadLocalRandom.current().nextInt(1, 7)
+            mDice2 = ThreadLocalRandom.current().nextInt(1, 7)
+
+            redrawDice()
+        }
+    }
+
+    private fun redrawDice() {
+        setDiceImage(iv_diceRight, mDice1)
+        setDiceImage(iv_diceLeft, mDice2)
+    }
+
+    private fun setDiceImage(iv_dice: ImageView, dice: Int) {
+        iv_dice.setImageDrawable(
+            ContextCompat.getDrawable(
+                this,
+                resources.getIdentifier(
+                    "dice" + dice,
+                    "drawable", packageName
+                )
+            )
+        )
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt("dice1", mDice1)
+        outState.putInt("dice2", mDice2)
+        super.onSaveInstanceState(outState)
     }
 }
