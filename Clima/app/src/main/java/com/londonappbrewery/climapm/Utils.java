6d59feaf1ab4c89639cfe631030cefc67d72614e
NG <nick.goupinets@gmail.com>
2020-04-21 20:16:56 -0400

    Initial Clima

---
+++ Clima/app/src/main/java/com/londonappbrewery/climapm/Utils.java
@@ -0,0 +1,20 @@
+package com.londonappbrewery.climapm;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Utils {
+
+    public static String getProperty(String key, Context context) throws IOException {
+        Properties properties = new Properties();
+        AssetManager assetManager = context.getAssets();
+        InputStream inputStream = assetManager.open("config.properties");
+        properties.load(inputStream);
+        return properties.getProperty(key);
+    }
+
+}
